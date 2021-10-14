c7030263fa74369a7b96f1ef563978770576ceb4
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-10-14 12:30:24 +0530

    Create template_c4_saa2.py

---
+++ template_c4_saa2.py
@@ -0,0 +1,11 @@
+# Defining a function 'square()' with 'side' as parameter
+
+    # Calculate 'area' 
+    
+    # Calculate 'volume'
+    
+    # Printing the results 'area' and 'volume'
+    
+
+  
+# Calling the function 'square()' with '8' as argument
